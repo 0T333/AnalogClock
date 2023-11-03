586f7ddc3767a80d3c65207fa0e089547a223542
0T333 <130041748+0T333@users.noreply.github.com>
2023-11-03 15:34:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const numberHours = document.querySelector('.number-hours');
+
+const numberElement = [];
+
+//creat numberhours
+for (let i = 1;i<=12; i++ ){
+    numberElement.push(
+        `<span style="--index:${i};"><p>${i}</p></span>`
+    );
+    
+}
+
+numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
